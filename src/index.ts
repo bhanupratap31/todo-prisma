90a98dbb6d72067b7ba069d4df1d4daf1b1b3f04
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-22 14:12:36 +0530

    insert update delete clubbed

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-// async function insertUser(username: string, password: string, firstName: string, lastName: string){
-//     const res = await prisma.user.create({
-//         data: {
-//             email: username, 
-//             password, 
-//             firstName, 
-//             lastName
-//         }
-//     })
-//     console.log(res);
-// }
-
-// insertUser("bhanups@gmail.com", "bhanu", "Bhanu", "Pratap");
+async function insertUser(username: string, password: string, firstName: string, lastName: string){
+    const res = await prisma.user.create({
+        data: {
+            email: username, 
+            password, 
+            firstName, 
+            lastName
+        }
+    })
+    console.log(res);
+}
 
 
-// interface UpdateParams{
-//     firstName: string;
-//     lastName: string;
-// }
 
-// async function updateUser(username: string, {
-//     firstName,
-//     lastName
-// }: UpdateParams){
-//     const res = await prisma.user.update({
-//         where: {email: username}, 
-//         data: {
-//             firstName, 
-//             lastName
-//         }
-//     })
-//     console.log(res);
-// }
 
-// updateUser("bhanups@gmail.com", {firstName: "bhanups", lastName: "prataps"});
+interface UpdateParams{
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams){
+    const res = await prisma.user.update({
+        where: {email: username}, 
+        data: {
+            firstName, 
+            lastName
+        }
+    })
+    console.log(res);
+}
+
+
 
 async function deleteUser(username: string){
     const res = await prisma.user.delete({
@@ -44,4 +44,7 @@ async function deleteUser(username: string){
     console.log(res); 
 }
 
+
+insertUser("bhanups@gmail.com", "bhanu", "Bhanu", "Pratap");
+updateUser("bhanups@gmail.com", {firstName: "bhanups", lastName: "prataps"});
 deleteUser("bhanups@gmail.com");

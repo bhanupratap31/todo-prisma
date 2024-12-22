cc60188310eb8d78a800e127ec2ab56286d3b93a
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-22 14:03:53 +0530

    Update Operation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string){
-    const res = await prisma.user.create({
+// async function insertUser(username: string, password: string, firstName: string, lastName: string){
+//     const res = await prisma.user.create({
+//         data: {
+//             email: username, 
+//             password, 
+//             firstName, 
+//             lastName
+//         }
+//     })
+//     console.log(res);
+// }
+
+// insertUser("bhanups@gmail.com", "bhanu", "Bhanu", "Pratap");
+
+
+interface UpdateParams{
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams){
+    const res = await prisma.user.update({
+        where: {email: username}, 
         data: {
-            email: username, 
-            password, 
             firstName, 
             lastName
         }
@@ -13,4 +35,4 @@ async function insertUser(username: string, password: string, firstName: string,
     console.log(res);
 }
 
-insertUser("bhanups@gmail.com", "bhanu", "Bhanu", "Pratap");
+updateUser("bhanups@gmail.com", {firstName: "bhanups", lastName: "prataps"});
